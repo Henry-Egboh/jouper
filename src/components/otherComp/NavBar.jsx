7bfc9f35775b4ee6e31acae876953002ea8e4ea5
Henry-Eg <107496624+Henry-Egboh@users.noreply.github.com>
2023-04-24 12:51:41 +0100

    Added nav links to the navbar

--- src/components/otherComp/NavBar.jsx
+++ src/components/otherComp/NavBar.jsx
@@ -1,16 +1,37 @@
-import { Avatar, HStack, Heading, Spacer, Flex } from "@chakra-ui/react";
+import { Avatar, HStack, Heading, Spacer, Flex, Icon, UnorderedList, ListItem, ListIcon, Link } from "@chakra-ui/react";
 import mine from "./henry.jpg";
+import { FaBlender, FaCottonBureau } from "react-icons/fa";
+import { NavLink as RouterLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <main>
       <Flex py={2} px={4} alignItems='center'>
         <HStack>
-          <Heading>Jouper</Heading>
+            <Icon as={FaCottonBureau} boxSize='2rem' />
+          <Heading fontSize='1.5rem'>Jouper</Heading>
         </HStack>
         <Spacer />
         <HStack>
           <Avatar objectFit="cover" src={mine} boxSize="40px" />
+          <UnorderedList pl='50px' listStyleType='none' display='flex' columnGap='3rem'>
+            <ListItem>
+                <ListIcon as={FaBlender} />
+                    <Link as={RouterLink} to='/'>Services</Link>
+            </ListItem>
+            <ListItem>
+                <ListIcon as={FaBlender} />
+                    <Link as={RouterLink} to='/'>Products</Link>
+            </ListItem>
+            <ListItem>
+                <ListIcon as={FaBlender} />
+                    <Link as={RouterLink} to='/create'>Create</Link>
+            </ListItem>
+            <ListItem>
+                <ListIcon as={FaBlender} />
+                    <Link as={RouterLink} to='/profile'>Profile</Link>
+            </ListItem>
+          </UnorderedList>
         </HStack>
       </Flex>
     </main>
